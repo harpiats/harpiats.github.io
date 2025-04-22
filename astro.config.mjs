f043a5b7201ffeb3ca37887232c9c60deb7325cf
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2025-04-22 07:19:54 -0300

    Add sitemap integration and robots.txt route
    - Integrated sitemap functionality into the Astro configuration.
    - Created a new route for serving robots.txt with dynamic sitemap URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,66 +2,66 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://harpiats.github.io",
-	integrations: [
-		starlight({
-			title: 'Harpia',
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/harpiats' },
-			],
-			components: {
-        // Override the default `LanguageSelect` component.
-        LanguageSelect: './src/components/VersionBadge.astro',
-      },
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Introduction', slug: 'guides/introduction' },
-						{ label: 'Installation', slug: 'guides/installation' },
-						{ label: 'Project Structure', slug: 'guides/project-structure' },
-						{ label: 'Environment Variables', slug: 'guides/environment-variables' },
-						{ label: 'Database', slug: 'guides/database' },
-						{ label: 'Commands and Scaffold', slug: 'guides/commands-and-scaffold' },
-						{ label: 'Module', slug: 'guides/modules' },
-						{ label: 'Seeding', slug: 'guides/seeding' },
-						{ label: 'Factory', slug: 'guides/factory' },
-						{ label: 'Tests', slug: 'guides/test' },
-						{ label: 'Observers', slug: 'guides/observers' },
-						{ label: 'Mailer', slug: 'guides/mailer' },
-						{ label: 'Tasks and Jobs', slug: 'guides/tasks-and-jobs' },
-						{ label: 'Utilities', slug: 'guides/utilities' },
-						{ label: 'S3', slug: 'guides/s3' },
-					],
-				},
-				{
-					label: 'Core',
-					items: [
-						{ label: 'Introduction', slug: 'core/introduction' },
-						{ label: 'Installation', slug: 'core/installation' },
-						{ label: 'Router', slug: 'core/router' },
-						{ label: 'Request', slug: 'core/request' },
-						{ label: 'Response', slug: 'core/response' },
-						{ label: 'Middleware', slug: 'core/middleware' },
-						{ label: 'Cookies', slug: 'core/cookies' },
-						{ label: 'Cache', slug: 'core/cache' },
-						{ label: 'Session', slug: 'core/session' },
-						{ label: 'Memory Storage', slug: 'core/memory-storage' },
-						{ label: 'Static Files', slug: 'core/static-files' },
-						{ label: 'Cors', slug: 'core/cors' },
-						{ label: 'Shield', slug: 'core/shield' },
-						{ label: 'Template Engine', slug: 'core/template-engine' },
-						{ label: 'Test Client', slug: 'core/test-client' },
-						{ label: 'Monitor', slug: 'core/monitor' },
-						{ label: 'Upload', slug: 'core/upload' },
-						{ label: 'Web Sockets', slug: 'core/web-socket' },
-					],
-				},
-				{ label: 'Deploy', link: '/deploy' },
-				{ label: 'Development Roadmap', link: '/roadmap' },
-			],
-		}),
-	],
+	integrations: [starlight({
+		title: 'Harpia',
+		social: [
+			{ icon: 'github', label: 'GitHub', href: 'https://github.com/harpiats' },
+		],
+		components: {
+    	// Override the default `LanguageSelect` component.
+    	LanguageSelect: './src/components/VersionBadge.astro',
+	  },
+		sidebar: [
+			{
+				label: 'Guides',
+				items: [
+					{ label: 'Introduction', slug: 'guides/introduction' },
+					{ label: 'Installation', slug: 'guides/installation' },
+					{ label: 'Project Structure', slug: 'guides/project-structure' },
+					{ label: 'Environment Variables', slug: 'guides/environment-variables' },
+					{ label: 'Database', slug: 'guides/database' },
+					{ label: 'Commands and Scaffold', slug: 'guides/commands-and-scaffold' },
+					{ label: 'Module', slug: 'guides/modules' },
+					{ label: 'Seeding', slug: 'guides/seeding' },
+					{ label: 'Factory', slug: 'guides/factory' },
+					{ label: 'Tests', slug: 'guides/test' },
+					{ label: 'Observers', slug: 'guides/observers' },
+					{ label: 'Mailer', slug: 'guides/mailer' },
+					{ label: 'Tasks and Jobs', slug: 'guides/tasks-and-jobs' },
+					{ label: 'Utilities', slug: 'guides/utilities' },
+					{ label: 'S3', slug: 'guides/s3' },
+				],
+			},
+			{
+				label: 'Core',
+				items: [
+					{ label: 'Introduction', slug: 'core/introduction' },
+					{ label: 'Installation', slug: 'core/installation' },
+					{ label: 'Router', slug: 'core/router' },
+					{ label: 'Request', slug: 'core/request' },
+					{ label: 'Response', slug: 'core/response' },
+					{ label: 'Middleware', slug: 'core/middleware' },
+					{ label: 'Cookies', slug: 'core/cookies' },
+					{ label: 'Cache', slug: 'core/cache' },
+					{ label: 'Session', slug: 'core/session' },
+					{ label: 'Memory Storage', slug: 'core/memory-storage' },
+					{ label: 'Static Files', slug: 'core/static-files' },
+					{ label: 'Cors', slug: 'core/cors' },
+					{ label: 'Shield', slug: 'core/shield' },
+					{ label: 'Template Engine', slug: 'core/template-engine' },
+					{ label: 'Test Client', slug: 'core/test-client' },
+					{ label: 'Monitor', slug: 'core/monitor' },
+					{ label: 'Upload', slug: 'core/upload' },
+					{ label: 'Web Sockets', slug: 'core/web-socket' },
+				],
+			},
+			{ label: 'Deploy', link: '/deploy' },
+			{ label: 'Development Roadmap', link: '/roadmap' },
+		],
+	}), sitemap()],
 });
